caf5fa1c44fb4565ff8a77b910bc3f2460b5be84
PhucDev02 <76049784+PhucDev02@users.noreply.github.com>
2024-05-09 10:22:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/th2_bottomnav/ListFragment.java
@@ -0,0 +1,21 @@
+package com.example.th2_bottomnav;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ListFragment extends Fragment {
+    ListFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/th2_bottomnav/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.th2_bottomnav;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.th2_bottomnav.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton floatingActionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton=findViewById(R.id.add_fab);
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        floatingActionButton.setOnClickListener(view ->{
+            Toast.makeText(MainActivity.this, "Click Add", Toast.LENGTH_SHORT
+            ).show();
+        });
+    }
+
+    ListFragment listFragment=new ListFragment();
+    FavoriteFragment favoriteFragment = new FavoriteFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId=item.getItemId();
+        if(itemId ==R.id.navigation_home)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,listFragment).commit();
+            return true;
+        }
+        if(itemId ==R.id.navigation_dashboard)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,favoriteFragment).commit();
+return true;
+        }
+        if(itemId ==R.id.navigation_notifications)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,searchFragment).commit();
+        return true;
+        }
+        return false;
+    }
+}
